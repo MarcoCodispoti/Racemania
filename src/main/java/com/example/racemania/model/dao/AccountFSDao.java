e2ae5033e2654329f7346a303d166809f61a792f
MarcoCodispoti <marco01codispoti@gmail.com>
2025-06-25 00:02:50 +0200

    rimozione issues minori

--- src/main/java/com/example/racemania/model/dao/AccountFSDao.java
+++ src/main/java/com/example/racemania/model/dao/AccountFSDao.java
@@ -11,31 +11,25 @@ public class AccountFSDao {
 
         try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/com/example/racemania/Users.txt"))) {
             String line;
-            System.out.println("File assoluto: " + file.getAbsolutePath());
 
             while ((line = reader.readLine()) != null) {
-                System.out.println("sono nel while");
                 String[] parts = line.split(";");
                 if (parts.length < 6) continue;
 
                 String fileuserIdStr = parts[0];
-                // String fileusername = parts[1];
                 String fileEmail = parts[2].trim();
                 String filePassword = parts[3].trim();
                 String fileRole = parts[4].trim();
                 String fileTrackIdStr = parts[5].trim();
-                System.out.println("Stampo i valori letti dal file: " + '\n' + fileuserIdStr + '\n' + fileRole + '\n' + fileTrackIdStr + '\n' + '\n');
 
 
                 if (email.equals(fileEmail) && password.equals(filePassword)) {
-                    System.out.println("Ho trovate un utente compatibile con email e password inserite");
 
                     int userId = Integer.parseInt(parts[0].trim());
                     String role = parts[4].trim();
                     String trackIdStr = parts[5].trim();
                     int trackId = trackIdStr.equalsIgnoreCase("NULL") ? -1 : Integer.parseInt(trackIdStr);
 
-                    System.out.println("Dettagli account inserito: " + '\n' + userId + '\n' + role + '\n' + trackId + '\n' + '\n');
 
                     account.setUserId(userId);
                     account.setRole(role);
@@ -43,7 +37,7 @@ public class AccountFSDao {
                     return account;
 
                 } else{
-                    System.out.println("Nessuna corrispondenza trovata");
+                    throw new FailedLoginException("Nessuna corrispondenza trovata");
                 }
             }
         } catch (IOException e) {
