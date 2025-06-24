0dee50b9a26faed802c3fc968ecfd9c8de3e9168
MarcoCodispoti <marco01codispoti@gmail.com>
2025-06-24 19:01:46 +0200

    rimozione issues

--- src/main/java/com/example/racemania/model/Instructor.java
+++ src/main/java/com/example/racemania/model/Instructor.java
@@ -1,4 +1,9 @@
 package com.example.racemania.model;
 
 public class Instructor {
+
+    public Instructor(){
+        // si può implementare successivamente per espandere l'applicazione
+    }
+
 }
