dcfe910585405598eb47406638e9884dd1bb315d
MarcoCodispoti <marco01codispoti@gmail.com>
2025-06-25 00:28:14 +0200

    rimozione issues minori

--- src/main/java/com/example/racemania/model/dao/TrackDao.java
+++ src/main/java/com/example/racemania/model/dao/TrackDao.java
@@ -33,8 +33,8 @@ public class TrackDao {
             try {
                 if (stmt != null)
                     stmt.close();
-            } catch (SQLException e) {
-                //not handled
+            } catch (SQLException _){
+                // not handled
             }
         }
         return tracks;
